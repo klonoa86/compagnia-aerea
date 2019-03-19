6d2d61ca0dd4595421fbd3c518f2b6a074cc05af
klonoa86 <robeliguori86@gmail.com>
2019-03-19 11:33:02 +0100

    daoimpl migliorato

--- CompagniaAerea/src/main/java/it/begear/CompagniaAerea/dao/DAOImpl.java
+++ CompagniaAerea/src/main/java/it/begear/CompagniaAerea/dao/DAOImpl.java
@@ -1,66 +1,91 @@
 package it.begear.CompagniaAerea.dao;
 
 import java.io.Serializable;
-
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+public class DAOImpl implements DAO {
+
+ private SessionFactory sessionFactory = null;
+
+ private static DAOImpl instance = null;
+
+ private DAOImpl() {
+  final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+  sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+ }
+
+ public static DAOImpl getInstance() {
+  if (instance == null) {
+   instance = new DAOImpl();
+  }
+  return instance;
+ }
+
+ Session createConnection() {
+  Session session = sessionFactory.openSession();
+  return session;
+ }
+
+ @Override
+ public <D> void create(D d) {
+  try (Session session = createConnection()) {
+   session.beginTransaction();
+   session.save(d);
+   session.getTransaction().commit();
+  } catch (HibernateException e) {
+   e.printStackTrace();
+  }
+ }
+
+ @Override
+ public <D> D read(Class<D> classe, Serializable pk) {
+  D d = null;
+  try (Session session = createConnection()) {
+   session.beginTransaction();
+   d = session.get(classe, pk);
+   session.getTransaction().commit();
+  } catch (HibernateException e) {
+   e.printStackTrace();
+  }
+  return d;
+ }
+
+ @Override
+ public <D> void update(D d) {
+  try (Session session = createConnection()) {
+   session.beginTransaction();
+   session.update(d);
+   session.save(d);
+   session.getTransaction().commit();
+  } catch (Exception e) {
+   e.printStackTrace();
+  }
+
+ }
+
+ @Override
+ public <D> void delete(D d) {
+  try (Session session = createConnection()) {
+   session.beginTransaction();
+   session.delete(d);
+   session.getTransaction().commit();
+  } catch (Exception e) {
+   e.printStackTrace();
+
+  }
 
 
-public class DAOImpl implements DAO{
-	SessionFactory sessionFactory = null;
-	final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
-	private static DAOImpl instance = null;
+ }
 
-	private DAOImpl() {
-		this.sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-	}
-
-	public static DAOImpl getInstance() {
-		if (instance == null) {
-			instance = new DAOImpl();
-		}
-		return instance;
-	}
-
-	public <T> void create(T entity) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		System.out.println("Inserimento...");
-		session.save(entity);
-		session.getTransaction().commit();
-		session.close();
-	}
-
-	public <T> void update(T entity) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		System.out.println("Aggiornamento...");
-		session.update(entity);
-		session.getTransaction().commit();
-		session.close();
-	}
-
-	public <T> void delete(T entity) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		System.out.println("Cancellazione...");
-		session.delete(entity);
-		session.getTransaction().commit();
-		session.close();
-	}
-
-	public <T> T read(Class<T> entity, Serializable id) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		System.out.println("Lettura...");
-		T t = session.get(entity, id);
-		session.getTransaction().commit();
-		session.close();
-		return t;
-	}
+// @Override
+// public <D> D LoadLazyRelations(Class<D> d, Serializable g) {
+//  
+//  return null;
+// }
 
 }
