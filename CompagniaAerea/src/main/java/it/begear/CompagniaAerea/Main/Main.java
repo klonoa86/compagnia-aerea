a0e6574d92afa2927bdc961354262b21fcd8040a
klonoa86 <robeliguori86@gmail.com>
2019-03-19 12:32:40 +0100

    modifica file onfigurazione

--- CompagniaAerea/src/main/java/it/begear/CompagniaAerea/Main/Main.java
+++ CompagniaAerea/src/main/java/it/begear/CompagniaAerea/Main/Main.java
@@ -1,9 +1,16 @@
 package it.begear.CompagniaAerea.Main;
 
+import it.begear.CompagniaAerea.Model.Cliente;
+import it.begear.CompagniaAerea.dao.DAO;
+import it.begear.CompagniaAerea.dao.DAOImpl;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		DAO dao= DAOImpl.getInstance();
+		
+		
+		
 	}
 
 }
