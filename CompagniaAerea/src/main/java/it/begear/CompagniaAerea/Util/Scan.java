dcedf3abeebee5dcd602eedbce4de4ca0139d04e
Mina <Mina@192.168.1.77>
2019-03-19 09:51:23 +0100

    Gestione scanner completa

--- CompagniaAerea/src/main/java/it/begear/CompagniaAerea/Util/Scan.java
+++ CompagniaAerea/src/main/java/it/begear/CompagniaAerea/Util/Scan.java
@@ -1,6 +1,26 @@
 package it.begear.CompagniaAerea.Util;
 
+import java.util.Scanner;
+
 public class Scan {
-	
-	
+
+	public static int scannerInteri() {
+		Scanner myInt = new Scanner(System.in);
+
+		if (myInt.hasNextInt()) {
+			return myInt.nextInt();
+		}
+		System.out.println("Errore inserisci un numero! ");
+		String svuota = myInt.nextLine();
+		return scannerInteri();
+
+	}
+
+	public static String scannerString() {
+		Scanner myString = new Scanner(System.in);
+
+		String scannerStringa = myString.nextLine();
+		return scannerStringa;
+	}
+
 }
